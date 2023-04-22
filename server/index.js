cf447ca10dfb4353a0f73858176ac115180be961
JimmyJohnes <amsoliman03@outlook.com>
2023-04-22 19:14:46 +0200

    last commit set up the front-end search as to search for animals by type

--- server/index.js
+++ server/index.js
@@ -7,11 +7,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-animals = [...Array(250).keys()].map(i => {
+let animals = [...Array(250).keys()].map(i => {
     return {
-        id,
+        i,
         name: chance.name(),
         age: chance.age(),
         type: chance.animal(),
     }
 });
+
+app.get('/animals', (req, res) => {
+    const query = req.query.q?.toLowerCase() || '';
+    const result = animals.filter(animal => animal.type.toLowerCase().includes(query));
+    res.send(result);
+});
+
+app.listen(8080,() => {console.log("listening on port 8080")})
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [animals,setAnimals] = useState([]);
+
+  useEffect(() => {
+    const lastQuery = localStorage.getItem('lastQuery');
+    search(lastQuery);
+  },[]);
+
+  const search = async (q) =>
+  {
+    const response = await fetch(`http://localhost:8080/animals?q=${q}`);
+    const data = await response.json();
+    setAnimals(data);
+    localStorage.setItem('lastQuery',q)
+  }
 
   return (
     <>
-      
+      <input
+      type='text'
+      onChange={(e) => search(e.target.value)}
+      placeholder='Enter Animal Type'
+      />
+      <ul>
+        {animals.map((animal) => {
+          return <li key={animal.i}>{animal.type}: {animal.name},  {animal.age} years old</li>
+        })}
+        {animals.length === 0 ? 'no animals found' : null}
+      </ul>
     </>
   )
 }
